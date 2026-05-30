4799c4b0de127920d0ea3f2f8f142b9e3c5d99b2
Dickson-Cobblah <2425401067@live.gctu.edu.gh>
2026-05-30 16:37:24 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Samuel Nii Anthony Dickson-Cobblah
+# Student  ID: 2425401067
+# Email:2425401067@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Samuel Nii Anthony Dickson-Cobblah, this programming assignment is my own
+# and have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters uding print statements.
+    print()
+    print("...SAD")
+    print()
 
 
+print("...SSSSSSSSSSSS.............AA..............DDDDDDD....")
+print("...SSSSSSSSSSSS...........AA..AA............DD....DD...")
+print("...SS.....................AA..AA............DD.....DD..")
+print("...SS.....................AA..AA............DD......DD.")
+print("...SSSSSSSSSS............AAAAAAAA...........DD......DD.")
+print(".............SS...........AA...AA...........DD......DD.")
+print(".............SS...........AA...AA...........DD......DD.")
+print(".............SS...........AA...AA...........DD......DD.")
+print("...SSSSSSSSSSSS...**......AA...AA.....**....DD.....DD..")
+print("...SSSSSSSSSSSS...**......AA...AA.....**....DDDDDDDD...")
+
+print()
 
 main()
